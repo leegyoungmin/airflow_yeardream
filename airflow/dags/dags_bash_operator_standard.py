68b3f9e80391202fa8ed0278650fe9e1e8f67c0e
이경민 <dlrudals8899@gmail.com>
2024-06-15 10:26:54 +0900

    feat : Dags Bash Operator Standard 생성

--- airflow/dags/dags_bash_operator_standard.py
+++ airflow/dags/dags_bash_operator_standard.py
@@ -1,9 +1,29 @@
 from airflow import DAG
 
-import datetime
+from airflow.operators.bash import BashOperator
+
+import pendulum
 
 dags_bash_operator_standard = DAG(
     dag_id='dags_bash_operator_standard',
-    start_date=datetime.datetime(2024, 6, 1),
-    tags=['homework']
+    start_date=pendulum.datetime(2024, 6, 1, tz="Asia/Seoul"),
+    tags=['homework'],
+    schedule='0 9 * * 1,5'
 )
+
+task_1 = BashOperator(
+    task_id='bash_task_1',
+    bash_command='echo task_1'
+)
+
+task_2 = BashOperator(
+    task_id='bash_task_2',
+    bash_command='echo task_2'
+)
+
+task_3 = BashOperator(
+    task_id='bash_task_3',
+    bash_command='echo task_3'
+)
+
+task_1 >> task_2 >> task_3
