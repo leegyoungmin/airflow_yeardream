724c89947fc5aadaed306267b9ccb2b577882586
이경민 <dlrudals8899@gmail.com>
2024-06-15 10:59:25 +0900

    feat : Dags Bash Operator Decolator 생성

---
+++ airflow/dags/dags_bash_operator_decorator.py
@@ -0,0 +1,31 @@
+from airflow.decorators import dag
+import pendulum
+
+from airflow.operators.bash import BashOperator
+
+@dag(
+    dag_id='dags_bash_operator_decorator',
+    start_date=pendulum.datetime(2024, 5, 1, tz="Asia/Seoul"),
+    schedule="0 9 * * 1,5",
+    tags=['homework'],
+    catchup=False
+)
+def generate_bash_dag():
+    task_1 = BashOperator(
+        task_id='bash_task_1',
+        bash_command='task_1'
+    )
+
+    task_2 = BashOperator(
+        task_id='bash_task_2',
+        bash_command='task_2'
+    )
+
+    task_3 = BashOperator(
+        task_id='bash_task_3',
+        bash_command='task_3'
+    )
+
+    task_1 >> task_2 >> task_3
+
+generate_bash_dag()
