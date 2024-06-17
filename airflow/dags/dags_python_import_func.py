8903c655b973772198b2af2c8d6bb33c7255dc69
이경민 <dlrudals8899@gmail.com>
2024-06-17 13:45:00 +0900

    refactor : 잘못된 패키지 설정 변경

--- airflow/dags/dags_python_import_func.py
+++ airflow/dags/dags_python_import_func.py
@@ -2,7 +2,7 @@ from airflow import DAG
 import pendulum
 import datetime
 from airflow.operators.python import PythonOperator
-from plugins.common.common_func import get_sftp
+from common.common_func import get_sftp
 
 with DAG(
         dag_id="dags_python_import_func",
